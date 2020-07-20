65a118bce6bb56ab77b87fec6e41b0d43db0f041
Akasan <daisuke1024akagawa@gmail.com>
2020-07-20 17:59:20 +0900

    プロジェクト ファイルを追加します。

---
+++ MatriX/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MatriX
+{
+    public static class Matrix
+    {
+        public static Type[] intZeros1d(int size)
+        {
+            Type[] zerosArray = new Type[size];
+            int intTemp = 0;
+            var temp = (Type)(object)intTemp;
+            
+            for (int i = 0; i < size; i++)
+            {
+                zerosArray[i] = temp;
+            }
+            return zerosArray;
+        }
+
+        public static Type[,] intZeros2d(int size1, int size2)
+        {
+            Type[,] zerosArray = new Type[size1, size2];
+            int i, j;
+            int intTemp = 0;
+            var temp = (Type)(object)intTemp;
+            for (i = 0; i < size1; i++)
+            {
+                for(j=0; j<size2; j++)
+                {
+                    zerosArray[i, j] = temp;
+                }
+            }
+            return zerosArray;
+        }
+
+        public static Type[,] transpose(Type[,] originalMat)
+        {
+            Type[,] transposedMat = new Type[originalMat.GetLength(1), originalMat.GetLength(0)];
+            int i, j;
+            for(i=0; i<originalMat.GetLength(0); i++)
+            {
+                for(j=0; j<originalMat.GetLength(1); j++)
+                {
+                    transposedMat[j, i] = originalMat[i, j];
+                }
+            }
+            return transposedMat;
+        }
+    }
+
+    public class Class1
+    {
+    }
+}
